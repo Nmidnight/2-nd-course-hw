76858b7158d3c0bb073ef14dbb3286b55d192e6b
Nmidnight <bynanamid46@gmail.com>
2025-05-30 16:44:39 +0300

    added js

---
+++ hw-2.js
@@ -0,0 +1,46 @@
+// Задание 1 
+
+let a = 10;
+alert(a);
+a = 20;
+console.log(a);
+
+// Задание 2
+
+let firstIphone = 2007;
+alert(firstIphone);
+
+// Задание 3
+
+let authorName = 'Брендан Эйх';
+alert(authorName);
+
+// Задание 4
+a = 10; 
+let b = 2;
+alert(a + b); 
+alert(a - b); 
+alert(a * b); 
+alert(a / b); 
+
+// Задание 5
+
+b = 2**5; 
+alert(b); 
+
+// Задание 6
+a = 9;
+b = 2;
+alert(a % b); 
+
+// Задание 7
+
+let num = 1;
+
+num += 5; 
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num--;
+alert(num);
