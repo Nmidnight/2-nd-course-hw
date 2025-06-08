2835f1cd8497f6d3efea12bc6cdbb335efdf31b9
Nmidnight <bynanamid46@gmail.com>
2025-06-08 21:37:12 +0300

    game added

--- hw-2.5.js
+++ hw-2.5.js
@@ -7,7 +7,7 @@ document.querySelector('.guess_num').addEventListener('click', function () {
     while (!guessed) {
         let userNum = Number(prompt('Введите число от 1 до 100'));
         if (isNaN(userNum)) {
-            alert('Вы ввели не число, попробуйте еще раз!');
+            alert('Вы ввели не число!');
             continue;
         }
         if (userNum < randomNum) {
